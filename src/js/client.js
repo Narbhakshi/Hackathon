cc18965b61dfac81b6ec71a8028571e474e9a722
Narbhakshi <MDADILS@amdocs.com>
2017-12-13 11:45:53 +0530

    added charts

--- src/js/client.js
+++ src/js/client.js
@@ -7,6 +7,7 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./components/Layout";
 import LoginPage from "./components/landingPage/LoginPage";
 import Dashboard from "./components/dashboard/DashboardMain";
+import TestChart from "./components/charts/TestChart";
 
 
 const app = document.getElementById('app');
@@ -19,6 +20,7 @@ ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={LoginPage}></Route>
     <Route path="dashboard" name="dashboard" component={Dashboard}></Route>
+    <Route path="testChart" name="testChart" component={TestChart}></Route>
   </Router>
 
 
